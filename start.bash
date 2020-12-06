ea63029b45d384ff7f4c82278b841e1d743bc754
Denick <1049485+apie@users.noreply.github.com>
2020-12-06 16:45:42 +0100

    update start.bash

--- start.bash
+++ start.bash
@@ -1,5 +1,20 @@
 #!/bin/bash
+set -e
+if [ ! -f 'app.py' ]; then
+  echo 'Only run from within the dir'
+  exit 1
+fi
+#if [ ! -f 'config.py' ]; then
+#  cp config.example config.py
+#  echo 'Please edit the config file: config.py'
+#  exit 1
+#fi
+if [ ! -d "venv" ]; then
+  virtualenv --python=python3 venv
+fi
 source venv/bin/activate
-pip3 install -r setup/requirements.txt
+pip3 install pip-tools
+pip-sync setup/requirements.txt
+
 gunicorn app:app --workers 2 --bind 0.0.0.0:8002 --reload
 
