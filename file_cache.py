fb2346812e5df924a1e02e21339e1bffe8057841
Denick <1049485+apie@users.noreply.github.com>
2021-01-21 17:51:14 +0100

    Allow subdir to use as cache for local testing

--- file_cache.py
+++ file_cache.py
@@ -6,14 +6,20 @@ from datetime import datetime, timedelta
 from pathlib import Path
 from functools import wraps
 
-SUBDIR = 'albumscrobbles'
+try:
+    # Local cache used for testing. To use it, create this subdir.
+    SUBDIR = Path('cache')
+    assert SUBDIR.exists()
+except AssertionError:
+    # Default cache location
+    SUBDIR = Path('/tmp/albumscrobbles')
 
 def get_filename(*args):
     # Truncate filename to a max length
     return '-'.join(arg.replace('/','-') for arg in args if arg)[:200]
 
 def get_from_cache(*args, func_name, keep_days=None) -> str:
-    filename = Path(f"/tmp/{SUBDIR}/{func_name}/{get_filename(*args)}")
+    filename = SUBDIR / Path(f"{func_name}/{get_filename(*args)}")
     # print(f"Getting {func_name} from file cache: {args} {keep_days}")
     if keep_days and datetime.fromtimestamp(filename.stat().st_mtime)+timedelta(days=keep_days) < datetime.now():
         print(f'Cache expired. Removing file. {func_name} {args}')
@@ -25,7 +31,7 @@ def get_from_cache(*args, func_name, keep_days=None) -> str:
 def update_cache(*args, func_name, result: str):
     assert isinstance(result, str), f'Cache can only be used for string results! Not for {type(result)}'
     # print(f"Updating {func_name} in file cache: {args} {result}")
-    path = Path(f"/tmp/{SUBDIR}/{func_name}")
+    path = SUBDIR / Path(func_name)
     path.mkdir(parents=True, exist_ok=True)
     with open(path / Path(get_filename(*args)), "w") as f:
         f.write(result)
