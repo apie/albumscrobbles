0baf96fc87339fabad2d129091d6c10cdc761ffa
Denick <1049485+apie@users.noreply.github.com>
2022-12-19 22:58:17 +0100

    disable extra features for now

--- start.bash
+++ start.bash
@@ -17,5 +17,5 @@ source venv/bin/activate
 pip3 install pip==22.3.1 pip-tools==6.12.1
 pip-sync setup/requirements.txt
 
-GOATCOUNTER=1 BLASTFROMTHEPAST=1 OVERVIEW=1 SUBSCRIPTION=1 RSS=1 gunicorn app:app --workers 12 --bind 0.0.0.0:8002 --reload --timeout 60
+GOATCOUNTER=1 BLASTFROMTHEPAST=0 OVERVIEW=0 SUBSCRIPTION=0 RSS=0 gunicorn app:app --workers 12 --bind 0.0.0.0:8002 --reload --timeout 60
 
