76ed6609d1afeba7543115a755ad777fc549f987
Denick <1049485+apie@users.noreply.github.com>
2024-01-24 10:04:57 +0100

    restart gunicorn after a number of requests to prevent memory leaks accumulating

--- start.bash
+++ start.bash
@@ -17,5 +17,5 @@ source venv/bin/activate
 pip3 install pip==23.1.2 pip-tools==6.14.0
 pip-sync setup/requirements.txt
 
-GOATCOUNTER=1 BLASTFROMTHEPAST=1 OVERVIEW=1 SUBSCRIPTION=1 RSS=1 gunicorn app:app --workers 12 --bind 0.0.0.0:8002 --timeout 60
+GOATCOUNTER=1 BLASTFROMTHEPAST=1 OVERVIEW=1 SUBSCRIPTION=1 RSS=1 gunicorn app:app --workers 12 --bind 0.0.0.0:8002 --timeout 60 --max-requests=100 --max-requests-jitter=10
 
