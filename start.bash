77d2ce17016d574688eb5aac3830ad326c0578f1
Denick <1049485+apie@users.noreply.github.com>
2024-01-24 09:20:55 +0100

    do not auto reload production gunicorn

--- start.bash
+++ start.bash
@@ -17,5 +17,5 @@ source venv/bin/activate
 pip3 install pip==23.1.2 pip-tools==6.14.0
 pip-sync setup/requirements.txt
 
-GOATCOUNTER=1 BLASTFROMTHEPAST=1 OVERVIEW=1 SUBSCRIPTION=1 RSS=1 gunicorn app:app --workers 12 --bind 0.0.0.0:8002 --reload --timeout 60
+GOATCOUNTER=1 BLASTFROMTHEPAST=1 OVERVIEW=1 SUBSCRIPTION=1 RSS=1 gunicorn app:app --workers 12 --bind 0.0.0.0:8002 --timeout 60
 
