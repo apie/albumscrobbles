88ee905a0a251e2e04b7c4a648d636a6f6760699
Denick <1049485+apie@users.noreply.github.com>
2025-01-07 16:22:12 +0100

    lint

--- tests.py
+++ tests.py
@@ -7,15 +7,15 @@ from scrape import username_regex
 class TestUserName(unittest.TestCase):
     def test_username_regex(self):
         username_list = [
-            ('a1', True),
-            ('a1234567890', True),
-            ('a1234567890-_', True),
-            ('a12345678912345', True),
-            ('a', False),
-            ('a()', False),
-            ('aa ', False),
-            ('123', False),
-            ('a123456789123456', False),
+            ("a1", True),
+            ("a1234567890", True),
+            ("a1234567890-_", True),
+            ("a12345678912345", True),
+            ("a", False),
+            ("a()", False),
+            ("aa ", False),
+            ("123", False),
+            ("a123456789123456", False),
         ]
         for username, valid in username_list:
             with self.subTest(username=username):
